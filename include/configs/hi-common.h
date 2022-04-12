f05a961e01894b7b557b25a8bc25e98ffb8e43bf
Dmitry Ermakov <de@dimer.org.ua>
2022-04-12 09:10:15 +0300

    Fix typo

--- include/configs/hi-common.h
+++ include/configs/hi-common.h
@@ -1,4 +1,4 @@
-#define CONFIG_BOOTARGS "mem=${osmem} console=ttyAMA0,115200 panic=20 root=/dev/mtdblock3 rootfstype=squashfs init=/init mtdparts=sfc:256k(boot),64k(env),2048k(kernel),5120k(rootfs),-(rootfs_data)"
+#define CONFIG_BOOTARGS "mem=${osmem} console=ttyAMA0,115200 panic=20 root=/dev/mtdblock3 rootfstype=squashfs init=/init mtdparts=hi_sfc:256k(boot),64k(env),2048k(kernel),5120k(rootfs),-(rootfs_data)"
 #define CONFIG_BOOTCOMMAND "setenv setargs setenv bootargs ${bootargs}; run setargs; sf probe 0; sf read 0x82000000 0x50000 0x200000; bootm 0x82000000"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
@@ -12,9 +12,9 @@
 #ifndef CONFIG_XM_COMPATIBLE
     #define CONFIG_ENV_OFFSET       0x40000
 #else
-    #define CONFIG_ENV_OFFSET       0x30000
+    #define CONFIG_ENV_OFFSET       0x40000
 #endif
-#define CONFIG_ENV_SIZE         0x10000
+#define CONFIG_ENV_SIZE         0x40000
 
 #undef CONFIG_SYS_PROMPT
 #define CONFIG_SYS_PROMPT	"OpenIPC # "
